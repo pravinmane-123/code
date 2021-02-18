2f6eeb9a5b5d2c6df11c2d3e927501abdc22f993
pravinmane-123 <72380879+pravinmane-123@users.noreply.github.com>
2021-02-18 22:48:29 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,14 +1,13 @@
 """
 https://github.com/pravinmane-123/code.git
 """
-""" 
+
 
 def info(firstname='pravin',age=30):
     print ('firstname',firstname)
     print ('age',age)
 info ('mane',10)
 
-"""
 def res(n):
     if (n == 0 or n == 1):
         return 1
